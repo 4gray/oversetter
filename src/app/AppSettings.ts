9204c82c6a265bbdbab87ba6a426c12b36b47a4a
4gray <serega05@gmail.com>
2017-07-05 20:56:57 +0200

    add project landing page

--- src/app/AppSettings.ts
+++
@@ -1,5 +0,0 @@
-export class AppSettings {
-	private static apiKey = localStorage.getItem('apiKey');
-	public static get API_KEY(): string { return this.apiKey; }
-	public static set API_KEY(key:string) { this.apiKey = key; }
-}
